0c90e10171f1d707820d0c4f6c33f753c5985e90
Fritz <lonon.fritz@gmail.com>
2016-11-09 01:49:16 +0800

    Finish Tomorrow Javascript Today (BABEL) Example jquery

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,13 +1,20 @@
 var $ = require('jquery');
-var Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+	payTaxes() {
+		console.log(this.name + " now owes $0 in taxes.");
+	}
+}
 
 alert("Testing gulp watch!");
 
 var john = new Person("John Doe", "blue");
 john.greet();
 
-var jane = new Person("Jane Smith", "green");
+var jane = new Adult("Jane Smith", "oranges");
 jane.greet();
+jane.payTaxes();
 
 $("h1").remove();
 
