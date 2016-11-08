5b42a054a4f73a4ca4795bf6d07a6a66617394ae
Fritz <lonon.fritz@gmail.com>
2016-11-09 04:03:35 +0800

    Finish hamburger menu icon

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,20 +1,3 @@
-var $ = require('jquery');
-import Person from './modules/Person';
+import MobileMenu from './modules/MobileMenu';
 
-class Adult extends Person {
-	payTaxes() {
-		console.log(this.name + " now owes $0 in taxes.");
-	}
-}
-
-alert("Testing gulp watch!");
-
-var john = new Person("John Doe", "blue");
-john.greet();
-
-var jane = new Adult("Jane Smith", "oranges");
-jane.greet();
-jane.payTaxes();
-
-$("h1").remove();
-
+var mobileMenu = new MobileMenu();
